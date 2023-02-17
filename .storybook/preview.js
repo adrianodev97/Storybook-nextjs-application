c2ed9107f3d5c383fc2e79834dd033ca8b77c6c3
Adriano Junior <adrianojunior.dev97@gmail.com>
2023-02-17 17:02:43 -0300

    feat: new functions

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,26 @@
 import "../styles/globals.css"
+
+import * as NextImage from "next/image";
+
+import { setupWorker, rest } from "msw"
+import { redirect } from "next/dist/server/api-utils";
+
+if(typeof global.process === "undefined") {
+  const worker = setupWorker(
+    rest.get("http://localhost:3000/api/hello", (req, res, ctx) => {
+      return redirect(ctx.json({name: "John Doh"}))
+    })
+  )
+  worker.start
+}
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized/>
+})
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
--- stories/pages/home.stories.jsx
+++ stories/pages/home.stories.jsx
@@ -4,7 +4,10 @@ import Home from "../../pages/index.js";
 export default {
   title: 'Pages/Home',
   component: Home,
+  args: {
+    name: "John Dor",
+  }
 };
 
-export const homePage = () => <Home />
-
+export const homePage = () => <Home name="John Doe"/>
+homePage.args = { name: "John Dough" }
